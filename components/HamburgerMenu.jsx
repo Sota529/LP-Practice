7774ebe81707492dd3d374d1912863cb0a6f4e66
Sota529 <za446131@gmail.com>
2021-05-14 15:08:03 +0900

    ✨ ハンバーガーメニュー動作確認

--- components/HamburgerMenu.jsx
+++ components/HamburgerMenu.jsx
@@ -2,10 +2,28 @@ import { Button } from "@chakra-ui/button";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { Box } from "@chakra-ui/layout";
 
-const HamburgerMenu = () => {
+import {
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+} from "@chakra-ui/react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const HamburgerMenu = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box py="auto">
-      <Button variant="ghost" colorScheme="white" _focus="none" display="block">
+      <Button
+        variant="ghost"
+        colorScheme="white"
+        _focus="none"
+        display="block"
+        onClick={onOpen}
+      >
         <HamburgerIcon
           color="whiteAlpha.900"
           boxSize="3em"
@@ -13,6 +31,35 @@ const HamburgerMenu = () => {
           display="inline-block"
         />
       </Button>
+      <Drawer
+        placement="left"
+        onClose={onClose}
+        isOpen={isOpen}
+        blockScrollOnMount={false}
+      >
+        <DrawerContent>
+          <DrawerCloseButton onClick={onClose} />
+          <DrawerBody mt="2em">
+            {props.menus.map((menu) => {
+              return (
+                <Button
+                  key={menu.id}
+                  variant="ghost"
+                  colorScheme="white"
+                  w="100%"
+                  py="2em"
+                  mt="1em"
+                  display="block"
+                  _focus="none"
+                  onClick={onClose}
+                >
+                  <AnchorLink href={menu.id}>{menu.value}</AnchorLink>
+                </Button>
+              );
+            })}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   );
 };
